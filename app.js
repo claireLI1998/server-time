d4f1607d7e8a584faddffe45370f7e37f12599ac
Ruolin Li <linlin19980821@gmail.com>
2020-10-10 13:55:16 -0700

    changed port

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ app.get('/', (req, res) => {
     res.send("homepage succeed");
 })
 
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
     console.log("connection succeed");
 });
 
